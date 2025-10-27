fed55cad86882436e116a10299aef28040f1e375
Vishwa Mihiranga <vishwamihiranga25@gmail.com>
2025-10-27 15:57:14 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const postgres = require('postgres');
+require('dotenv').config();
 
 const sql = postgres({
-  host: 'ep-cool-truth-a1nvfpxx.ap-southeast-1.pg.koyeb.app',
-  database: 'koyebdb',
-  username: 'koyeb-adm',
-  password: 'npg_TZFIqH73wmQa',
-  ssl: 'require',
+  host: process.env.PGHOST || 'ep-cool-truth-a1nvfpxx.ap-southeast-1.pg.koyeb.app',
+  database: process.env.PGDATABASE || 'koyebdb',
+  username: process.env.PGUSER || 'koyeb-adm',
+  password: process.env.PGPASSWORD || 'npg_TZFIqH73wmQa',
+  ssl: process.env.PGSSL === 'require' ? 'require' : false,
   max: 10,
   idle_timeout: 20,
   connect_timeout: 10,
